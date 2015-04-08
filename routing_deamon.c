cfd5bee1cbdbd835a00e2268c97df0c9738b2305
kiran4399 <kiran4399@gmail.com>
2015-04-08 23:56:28 +0530

    commits

--- routing_deamon.c
+++ routing_deamon.c
@@ -21,8 +21,42 @@ int main(){
     fileEntry* Files;
     int num;
 
+	int localSocket;
+	int cgiSocket;
+	struct sockaddr_in cgiAddr;
+	unsigned int cgiAddrLength;
+	char buffer[10];
+	ssize_t re;
+	memset(buffer, '\0', 10);
+	char method[10];
+	char objectName[100];
+	int objLength;
+	int pathLength;
+	char path[100];
+	char response[256];
+	fd_set sockList, master;
+	int fdmax;
+	char* clientArr[1024];
+	int p;
+	
+	
+	
+	// these are for UDP listening
+    int remoteSocket;
+    struct sockaddr_in udpAddr, outAddr;
+	unsigned int udpAddrLength;
+	char ttlUDP;
+	short typeUDP;
+	int senderIdUDP;
+	int seqNumberUDP;
+	int numLinksUDP;
+	int numFilesUDP;
 
     //these are for ``
+
+	//parse arguments 
+	nodeID = atoi(argv[1]);
+	
 }
 
 
